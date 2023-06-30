f32e58a317358e2c3d8ec7ce365b92e1e1e24d45
Akshit Bhati <akshitworks24@gmail.com>
2023-06-30 11:32:27 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import {
   SearchIcon,
   PersonOutlineOutlinedIcon,
-  FavoriteBorderOutlinedIcon,
+  
   ShoppingBagOutlinedIcon,
 } from "assets";
 import "./navbar.css";
